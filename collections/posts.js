3e741c176e86106f9105a2acd450e1ad64323b75
Rob Dooh <rob.dooh@threestonestudios.com>
2015-11-27 10:26:09 -0500

    Seed posts with single example

--- collections/posts.js
+++ collections/posts.js
@@ -26,34 +26,36 @@ PostsSchema = new SimpleSchema({
 
 Posts.attachSchema( PostsSchema );
 
-// Allow
-Posts.allow({
-  insert: function(){
-    // Disallow inserts on the client by default.
-    return false;
-  },
-  update: function(){
-    // Disallow updates on the client by default.
-    return false;
-  },
-  remove: function(){
-    // Disallow removes on the client by default.
-    return false;
-  }
-});
+if(Meteor.isClient){
+  // Allow
+  Posts.allow({
+    insert: function(){
+      // Disallow inserts on the client by default.
+      return false;
+    },
+    update: function(){
+      // Disallow updates on the client by default.
+      return false;
+    },
+    remove: function(){
+      // Disallow removes on the client by default.
+      return false;
+    }
+  });
 
-// Deny
-Posts.deny({
-  insert: function(){
-    // Deny inserts on the client by default.
-    return true;
-  },
-  update: function(){
-    // Deny updates on the client by default.
-    return true;
-  },
-  remove: function(){
-    // Deny removes on the client by default.
-    return true;
-  }
-});
+  // Deny
+  Posts.deny({
+    insert: function(){
+      // Deny inserts on the client by default.
+      return true;
+    },
+    update: function(){
+      // Deny updates on the client by default.
+      return true;
+    },
+    remove: function(){
+      // Deny removes on the client by default.
+      return true;
+    }
+  });
+}
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,10 +1,25 @@
-if (Meteor.users.find().count() === 0) {
-  Accounts.createUser({
-    email: 'rob@company.com',
-    password: 'rob'
-  });
-  Accounts.createUser({
-    email: 'joe@company.com',
-    password: 'joe'
-  });
-}
+
+Meteor.startup(function(){
+  if (Meteor.users.find().count() === 0) {
+    Accounts.createUser({
+      email: 'rob@company.com',
+      password: 'rob',
+      _id: 'asdfasdfasdfads'
+    });
+    Accounts.createUser({
+      email: 'joe@company.com',
+      password: 'joe',
+      _id: 'jgjgjhgjhghgkjh'
+    });
+  }
+
+  if (Posts.find().count() === 0) {
+    Posts.insert({
+      "title": 'Why I Love Meteor',
+      "description": 'This is a cool blog post',
+      createdAt: ( new Date(2015, 3, 14 ).getTime() / 1000 ),
+      updatedAt: ( new Date(2015, 5, 18 ).getTime() / 1000 ),
+      "ownerId": 'asdfasdfasdfads'
+    });
+  }
+});
