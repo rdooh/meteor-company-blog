b46dcc28f9b5b3a5201969e046863979216d7d51
Rob Dooh <rob.dooh@threestonestudios.com>
2015-11-27 10:13:19 -0500

    add users file, add collection2 and helpers

--- collections/posts.js
+++ collections/posts.js
@@ -1,6 +1,31 @@
 Posts = new Meteor.Collection('posts');
 // Probably force to server methods for interactions
 
+PostsSchema = new SimpleSchema({
+  "title": {
+    type: String,
+    label: "Title"
+  },
+  "description": {
+    type: String,
+    label: "Description"
+  },
+  createdAt: {
+    type: Date,
+    label: "Creation Date"
+  },
+  updatedAt: {
+    type: Date,
+    label: "Last Updated"
+  },
+  "ownerId": {
+    type: String,
+    label: "Post Author"
+  }
+});
+
+Posts.attachSchema( PostsSchema );
+
 // Allow
 Posts.allow({
   insert: function(){
