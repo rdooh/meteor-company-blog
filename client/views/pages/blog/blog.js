109d5a8e132590ab878a223628509b1fd7fea8e9
Rob Dooh (iMac) <rob.dooh@threestonestudios.com>
2015-11-28 13:09:28 -0500

    Revise all templates

--- client/views/pages/blog/blog.js
+++ client/views/pages/blog/blog.js
@@ -3,6 +3,9 @@ if (Meteor.isClient) {
   Template.blog.helpers({
     ownBlog: function() {
       return this._id === Meteor.userId();
+    },
+    blogName: function() {
+      return this.username + "'s Blog";
     }
   });
 }
