fa7de5f75ee1d560e5b22a3fb1b2a884b6ecaba8
Rob Dooh (iMac) <rob.dooh@threestonestudios.com>
2015-11-28 12:03:47 -0500

    navigation set up on sidebar

--- client/views/common/navigation.js
+++ client/views/common/navigation.js
@@ -3,4 +3,16 @@ Template.navigation.rendered = function(){
     // Initialize metisMenu
     $('#side-menu').metisMenu();
 
-}; 
+};
+
+
+if (Meteor.isClient) {
+  Template.navigation.helpers({
+    bloggers: function() {
+      bloggers = Meteor.users.find({}, {fields: {username: 1}});
+      if(bloggers) {
+        return bloggers;
+      }
+    }
+  });
+}
