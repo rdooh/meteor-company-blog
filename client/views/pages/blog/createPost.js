ff37858adae2229ed9d0f78540705084fda6930c
Rob Dooh (iMac) <rob.dooh@threestonestudios.com>
2015-11-28 15:04:32 -0500

    add slug preview functionality

--- client/views/pages/blog/createPost.js
+++ client/views/pages/blog/createPost.js
@@ -2,7 +2,20 @@ if (Meteor.isClient) {
 
   Meteor.subscribe('allposts'); // this is already done - look into refactor
 
+  Template.createPost.helpers({
+    // inputTitle: function () { return Session.get('inputTitle'); },
+    postSlugPreview: function () { return Session.get('postSlugPreview'); }
+  });
+
+
   Template.createPost.events({
+    'keyup #title': function(event) {
+      console.log('test');
+      let inputTitle = $(event.target).val();
+      console.log(inputTitle);
+      let postSlugPreview = App.Utils.formatSlug(inputTitle);
+      Session.set('postSlugPreview',postSlugPreview);
+    },
     'click button': function(e, t) {
       if(Meteor.user()){
         let ownerId = Meteor.userId();
@@ -25,5 +38,7 @@ if (Meteor.isClient) {
         return Router.go('/'+username+'/'+slug);
       }
     }
+
+
   });
 }
